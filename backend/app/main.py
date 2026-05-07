4c0753ec2d9a6efefd1e56dbe9cb12cc11850175
martinmaldonado <3014683970101@ingenieria.usac.edu.gt>
2026-05-07 12:18:55 -0600

    feat(add):✨  define the basic structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,26 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
+
 import os
 
 app = Flask(__name__)
 
-# Configuración de la BD
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+# Importar rutas
+from routes.products    import productos_bp
+from routes.orders      import pedidos_bp
+from routes.returns import devoluciones_bp
+
+app.register_blueprint(productos_bp,    url_prefix='/api/productos')
+app.register_blueprint(pedidos_bp,      url_prefix='/api/pedidos')
+app.register_blueprint(devoluciones_bp, url_prefix='/api/devoluciones')
 
 @app.route('/health')
 def health():
